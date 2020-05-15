fcac4463700ac82ffc8ca065b78a77ea7f278930
benebsiny <stu995106@hotmail.com.tw>
2020-05-15 08:43:25 +0800

    Admin page synchronize to the mysql
    1. Add 'booking' app to the project settings
    2. Shows some tables at the django admin page

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Station, Train, Person, Ticket
 
 # Register your models here.
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(Person)
+admin.site.register(Ticket)

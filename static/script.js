7edd4f20f4b8d842f08b1effe6ee83eef9d2a2a3
林則穎 <dragon95070@gmail.com>
2020-05-20 13:21:17 +0800

    Create station button
    According to city,create station button

--- static/script.js
+++ static/script.js
@@ -1,25 +1,22 @@
-$('form').on('submit', function (event) {
-    event.preventDefault();
-
-    let stationName;
-    $(this).serializeArray().forEach(element => { // Get values from 'form'
-        if (element['name'] === 'station')
-            stationName = element['value'];
-    });
-
-
-    $.ajax({ // Asyn request
-        url: `api/station?sname=${stationName}`,
-        success: function (data) { // When request success
-            const station = data[0]; // Data
-        },
-        error: function (data) { // An error occurred
-
-        }
-    });
-});
-
 let stations, counties;
+
+async function init() {
+    stations = await getData();
+    counties = getCounty();
+    console.log(stations)
+
+    const beginBlock = $('.begin-block .county');
+    const destBlock = $('.dest-block .county');
+
+    counties.forEach(county => {
+        $(`<button class="btn btn-outline-primary btn-sm county-btn" type="button">${county}</button>`).appendTo(beginBlock);
+        $(`<button class="btn btn-outline-danger btn-sm county-btn" type="button">${county}</button>`).appendTo(destBlock);
+    });
+
+
+
+}
+init();
 
 function getData() {
     return new Promise(((resolve, reject) => {
@@ -42,36 +39,64 @@ function getCounty() {
     return counties;
 }
 
-async function init() {
-    stations = await getData();
-    counties = getCounty();
 
-    const beginBlock = $('.begin-block .county');
-    const destBlock = $('.dest-block .county');
+$('form').on('submit', function (event) {
+    event.preventDefault();
 
-    counties.forEach(county => {
-        $(`<button class="btn btn-outline-primary btn-sm county-btn" type="button">${county}</button>`).appendTo(beginBlock);
-        $(`<button class="btn btn-outline-danger btn-sm county-btn" type="button">${county}</button>`).appendTo(destBlock);
+    let stationName;
+    $(this).serializeArray().forEach(element => { // Get values from 'form'
+        if (element['name'] === 'station')
+            stationName = element['value'];
     });
-}
 
-init();
 
-$(document).on('click', '.dest-station-select-btn', function () {
-    const block = $('.dest-station-select');
-    block.css({left: $(this).offset().left, top: $(this).offset().top + 38})
-    block.toggle();
-})
+    $.ajax({ // Asyn request
+        url: `api/station?sname=${stationName}`,
+        success: function (data) { // When request success
+            const station = data[0]; // Data
+        },
+        error: function (data) { // An error occurred
+
+        }
+    });
+});
 
 $(document).on('click', '.county-btn', function () {
+    //let bottom in it's siblings only bright one
     $(this).siblings().removeClass('active');
-    $(this).toggleClass('active');
+    $(this).addClass('active');
+
+    $(".station").empty();
+
+    let station_filter = [];
+
+    for (let i = 0;i <stations.length ;i ++){
+        if ($(this).text().indexOf(stations[i]['cty'])!==-1){
+            station_filter.push(stations[i]['sname']);
+        }
+    }
+    console.log(station_filter)
+    const beginBlock_station = $('.begin-block .station');
+
+
+
+    station_filter.forEach(station=>{
+        $(`<button class="btn btn-outLine-primary btn-sm station-btn" type="button">${station}</button>`).appendTo(beginBlock_station);
+    });
+
+    //btn-outLine-primary btn-sm
+    // console.log($(this).text())
 })
 
-$(document).on('click', '.dropdown-menu', function (e) {
+$(document).on('click', '.station-btn', function () {
+    $(this).siblings().removeClass('station-btn');
+})
 
-    // When user clicked on county button, don't hide the dropdown menu
-    if(!$(e.target).hasClass('station-btn')){
-        e.stopPropagation();
+$(document).on('click', '.dropdown-menu', function (event) {
+    // When user not clicked on the station buttons, don't hide the dropdown menu
+    if(!$(event.target).hasClass('station-btn')){
+        event.stopPropagation();
     }
 });
+
+
